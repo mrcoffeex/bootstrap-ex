450e9f99cd66aa435fc25ebbac4bb838bc020012
mrcoffeex <kjohn0319@gmail.com>
2026-09-14 19:04:24 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap.min.css">
+    <title>Bootstrap 5</title>
+</head>
+<body>
+
+    
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
